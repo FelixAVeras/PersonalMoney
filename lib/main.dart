d9e0ea49f0da8fb919825db10dc7f2ac7a1b8aa3
Felix Veras <fcarvajal44@gmail.com>
2023-12-17 19:28:43 -0400

    Updating project to new version

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:personalmoney/bloc/provider.dart';
-// import 'package:personalmoney/bloc/register_provider.dart';
 import 'package:personalmoney/pages/homepage.dart';
-import 'package:personalmoney/pages/loginpage.dart';
-import 'package:personalmoney/pages/registerpage.dart';
 
-import 'package:personalmoney/helpers/databasehelper.dart';
-// void main() => runApp(PersonalMoney());
-
-void main() async {
-  await DbConn;
-  runApp(PersonalMoney());
-}
+void main() => runApp(const PersonalMoney());
 
 class PersonalMoney extends StatelessWidget {
+  const PersonalMoney({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return CustomProvider(
-        child: MaterialApp(
-      title: 'Personal Money',
-      theme: ThemeData(primarySwatch: Colors.teal),
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // initialRoute: 'Home',
-      // routes: {
-      //   'login': (BuildContext context) => LoginPage(),
-      //   'register': (BuildContext context) => RegisterPage(),
-      //   'Home': (BuildContext context) => HomePage(),
-      // },
-      home: HomePage(),
-    ));
+      title: 'Personal Money',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+      ),
+      // initialRoute: '/login',
+      initialRoute: '/home',
+      routes: {
+        // '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+      },
+    );
   }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,114 +1,135 @@
+// import 'package:flutter/material.dart';
+// import 'package:personalmoney/bloc/transactions_bloc.dart';
+// import 'package:personalmoney/pages/currencyPage.dart';
+// import 'package:personalmoney/pages/dashboradpage.dart';
+// import 'package:personalmoney/pages/historypage.dart';
+// import 'package:flutter/widgets.dart';
+// import 'package:personalmoney/pages/loginpage.dart';
+// import 'package:personalmoney/pages/myprofile.dart';
+// import 'package:personalmoney/pages/transactionpage.dart';
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _HomePageState createState() => _HomePageState();
+// }
+
+// class _HomePageState extends State<HomePage> {
+//   String currentAddress;
+//   int currentIndex = 0;
+
+//   // final transactionBloc = new TransactionsBloc();
+
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//         length: 2,
+//         child: Scaffold(
+//           appBar: AppBar(
+//           //   title: Text("Personal Money"),
+//           //   // centerTitle: true,
+//           //   // bottom: TabBar(
+//           //   //   tabs: [
+//           //   //     Tab(/*icon: Icon(Icons.home),*/ text: 'Inicio'),
+//           //   //     Tab(/*icon: Icon(Icons.calendar_today),*/ text: 'Historial'),
+//           //   //     // Tab(/*icon: Icon(Icons.sync_alt),*/ text: 'Convertidor Div.'),
+//           //   //   ],
+//           //   //   indicatorWeight: 4,
+//           //   //   indicatorColor: Colors.white,
+//           //   // ),
+//           //   // actions: [
+//           //   //   IconButton(
+//           //   //     icon: Icon(Icons.add),
+//           //   //     onPressed: () {
+//           //   //       Navigator.push(
+//           //   //           context,
+//           //   //           MaterialPageRoute(
+//           //   //               builder: (context) => TransactionPage()));
+//           //   //     },
+//           //   //     tooltip: 'Nueva Transaccion',
+//           //   //   )
+//           //   // ],
+//           // ),
+//           // body: _loadPage(currentIndex),
+//           // body: DashboardPage(),
+//           // body: TabBarView(
+//           //     children: [DashboardPage(), HistoryPage() /*, CurrencyPage()*/]),
+//           body: HistoryPage(),
+//           // drawer: Drawer(
+//           //   child: ListView(
+//           //     padding: EdgeInsets.zero,
+//           //     children: [
+//           //       DrawerHeader(
+//           //         decoration: BoxDecoration(
+//           //           color: Theme.of(context).primaryColor,
+//           //         ),
+//           //         child: Text('Personal Money - Menu',
+//           //             style: TextStyle(color: Colors.white, fontSize: 24)),
+//           //       ),
+//           //       ListTile(
+//           //         leading: Icon(Icons.person),
+//           //         title: Text('Mi Perfil'),
+//           //         onTap: () => {
+//           //           Navigator.of(context).pop(),
+//           //           Navigator.push(
+//           //               context,
+//           //               MaterialPageRoute(
+//           //                   builder: (context) => MyProfilePage()))
+//           //         },
+//           //       ),
+//           //       Divider(),
+//           //       ListTile(
+//           //         leading: Icon(Icons.settings),
+//           //         title: Text('Configuración'),
+//           //         onTap: () {},
+//           //       ),
+//           //       Divider(),
+//           //       ListTile(
+//           //         leading: Icon(Icons.logout),
+//           //         title: Text('Cerrar Sesión'),
+//           //         onTap: () => {
+//           //           Navigator.of(context).pop(),
+//           //           Navigator.push(context,
+//           //               MaterialPageRoute(builder: (context) => LoginPage()))
+//           //         },
+//           //       )
+//           //     ],
+//           //   ),
+//           // ),
+//           // bottomNavigationBar: _customBottomNavigationBar(),
+//           // floatingActionButton: FloatingActionButton.extended(
+//           //   label: Text('Nueva Transacción'),
+//           //   icon: Icon(Icons.add),
+//           //   onPressed: () => {
+//           //     Navigator.push(context,
+//           //         MaterialPageRoute(builder: (context) => TransactionPage()))
+//           //   },
+//           // ),
+//         ));
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:personalmoney/bloc/transactions_bloc.dart';
-import 'package:personalmoney/pages/currencyPage.dart';
-import 'package:personalmoney/pages/dashboradpage.dart';
-import 'package:personalmoney/pages/historypage.dart';
-import 'package:flutter/widgets.dart';
-import 'package:personalmoney/pages/loginpage.dart';
-import 'package:personalmoney/pages/myprofile.dart';
-import 'package:personalmoney/pages/transactionpage.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  String currentAddress;
-  int currentIndex = 0;
-
-  // final transactionBloc = new TransactionsBloc();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          // appBar: AppBar(
-          //   title: Text("Personal Money"),
-          //   // centerTitle: true,
-          //   // bottom: TabBar(
-          //   //   tabs: [
-          //   //     Tab(/*icon: Icon(Icons.home),*/ text: 'Inicio'),
-          //   //     Tab(/*icon: Icon(Icons.calendar_today),*/ text: 'Historial'),
-          //   //     // Tab(/*icon: Icon(Icons.sync_alt),*/ text: 'Convertidor Div.'),
-          //   //   ],
-          //   //   indicatorWeight: 4,
-          //   //   indicatorColor: Colors.white,
-          //   // ),
-          //   // actions: [
-          //   //   IconButton(
-          //   //     icon: Icon(Icons.add),
-          //   //     onPressed: () {
-          //   //       Navigator.push(
-          //   //           context,
-          //   //           MaterialPageRoute(
-          //   //               builder: (context) => TransactionPage()));
-          //   //     },
-          //   //     tooltip: 'Nueva Transaccion',
-          //   //   )
-          //   // ],
-          // ),
-          // body: _loadPage(currentIndex),
-          // body: DashboardPage(),
-          // body: TabBarView(
-          //     children: [DashboardPage(), HistoryPage() /*, CurrencyPage()*/]),
-          body: HistoryPage(),
-          // drawer: Drawer(
-          //   child: ListView(
-          //     padding: EdgeInsets.zero,
-          //     children: [
-          //       DrawerHeader(
-          //         decoration: BoxDecoration(
-          //           color: Theme.of(context).primaryColor,
-          //         ),
-          //         child: Text('Personal Money - Menu',
-          //             style: TextStyle(color: Colors.white, fontSize: 24)),
-          //       ),
-          //       ListTile(
-          //         leading: Icon(Icons.person),
-          //         title: Text('Mi Perfil'),
-          //         onTap: () => {
-          //           Navigator.of(context).pop(),
-          //           Navigator.push(
-          //               context,
-          //               MaterialPageRoute(
-          //                   builder: (context) => MyProfilePage()))
-          //         },
-          //       ),
-          //       Divider(),
-          //       ListTile(
-          //         leading: Icon(Icons.settings),
-          //         title: Text('Configuración'),
-          //         onTap: () {},
-          //       ),
-          //       Divider(),
-          //       ListTile(
-          //         leading: Icon(Icons.logout),
-          //         title: Text('Cerrar Sesión'),
-          //         onTap: () => {
-          //           Navigator.of(context).pop(),
-          //           Navigator.push(context,
-          //               MaterialPageRoute(builder: (context) => LoginPage()))
-          //         },
-          //       )
-          //     ],
-          //   ),
-          // ),
-          // bottomNavigationBar: _customBottomNavigationBar(),
-          // floatingActionButton: FloatingActionButton.extended(
-          //   label: Text('Nueva Transacción'),
-          //   icon: Icon(Icons.add),
-          //   onPressed: () => {
-          //     Navigator.push(context,
-          //         MaterialPageRoute(builder: (context) => TransactionPage()))
-          //   },
-          // ),
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Personal Money"),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(child: const Text('Pantalla de Home')),
+      floatingActionButton: FloatingActionButton.extended(
+        label: Text('Nueva Transacción'),
+        icon: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
   }
 }
